3d2ac796b711b3b25fe77464dcbf061df963766c
hojinlee <infohojin@gmail.com>
2024-05-20 19:27:01 +0900

    temp

---
+++ database/migrations/2022_03_09_031201_create_user_money_table.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * 회원 예치금
+ */
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_money', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('ref')->default(0);
+            $table->integer('level')->default(0);
+            $table->integer('pos')->default(1);
+
+            $table->bigInteger('user_id');
+            $table->string('email');
+
+            $table->string('currency')->nullable();
+            $table->decimal('balance', $precision = 8, $scale = 2)->default(0.0);
+
+            // 환불 계좌정보
+            $table->string('bank_name')->nullable();
+            $table->string('bank_user')->nullable();
+            $table->string('bank_account')->nullable();
+            $table->string('bank_swift')->nullable();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_money');
+    }
+};
---
+++ database/migrations/2022_04_17_065113_create_shop_wish_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shop_wish', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('ref')->default(0);
+            $table->integer('level')->default(0);
+            $table->integer('pos')->default(1);
+
+            $table->string('email'); //주문자정보
+
+            ## 상품정보
+            $table->bigInteger('product_id'); // 제품 번호
+            $table->string('product'); // 제품명
+            $table->string('image')->nullable();
+
+            ##
+            $table->string('expire')->nullable();   // 만료일자시 삭제
+            $table->string('later')->nullable();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shop_wish');
+    }
+};
