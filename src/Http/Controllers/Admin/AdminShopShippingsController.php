7c064b26ab51c2d42af233e9504b17dffdf3f618
hyoon1129 <hyoon1129@naver.com>
2024-06-26 17:20:55 +0900

    shipping 테이블 admin 페이지

---
+++ src/Http/Controllers/Admin/AdminShopShippingsController.php
@@ -0,0 +1,36 @@
+<?php
+namespace Jiny\Shop\Order\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+
+/**
+ * 라우트경로 : /admin/shop/shipping
+ */
+use Jiny\WireTable\Http\Controllers\WireTablePopupForms;
+class AdminShopShippingsController extends WireTablePopupForms
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setVisit($this);
+
+        ##
+        $this->actions['table'] = "shop_shippings"; // 테이블 정보
+        $this->actions['title'] = "관리자: 배송 관리";
+        $this->actions['subtitle'] = "배송 관리 페이지입니다.";
+
+        // 테이블을 출력하는 목록 blade입니다.
+        $this->actions['view']['list'] = "jiny-shop-order::admin.shop_shippings.list";
+
+        // 신규 데이터 입력 및 수정폼 입니다.
+        $this->actions['view']['form'] = "jiny-shop-order::admin.shop_shippings.form";
+    }
+
+
+
+}
