0adc7ba49a2f6b0f88db6adae0cb475511cf163a
hyeyoon <hyoon1129@naver.com>
2024-08-16 00:05:47 +0900

    refactor: 중복되는 함수 수정
    - Helper에 작성된 generateUnuqueCartId() 호출하게 수정

--- src/Http/Livewire/CartOffcanvas.php
+++ src/Http/Livewire/CartOffcanvas.php
@@ -74,10 +74,7 @@ class CartOffcanvas extends Component
 
     public function generateUniqueCartId()
     {
-        // 고유의 ID를 생성
-        $id = uniqid(mt_rand(), true);
-        $code = substr(hash('sha256',$id),0,15); // 10자리 추출
-        return date("Ymd-his")."-".$code;
+        return generateUniqueCartId();
     }
 
     public function incrementQuantity($id)
--- src/Http/Livewire/ShopCartItem.php
+++ src/Http/Livewire/ShopCartItem.php
@@ -63,10 +63,7 @@ class ShopCartItem extends Component
 
     public function generateUniqueCartId()
     {
-        // 고유의 ID를 생성
-        $id = uniqid(mt_rand(), true);
-        $code = substr(hash('sha256',$id),0,15); // 10자리 추출
-        return date("Ymd-his")."-".$code;
+        return generateUniqueCartId();
     }
 
     public function render()
