6f6f88f9f5d35a25eb4cb022cf5ecab7e0e71035
hyeyoon <hyoon1129@naver.com>
2024-08-30 14:51:23 +0900

    refactor: cart, wish 관리자 화면 레이아웃 개선

--- resources/views/admin/shop_cart/list.blade.php
+++ resources/views/admin/shop_cart/list.blade.php
@@ -1,43 +1,42 @@
-<x-wire-table>
+<x-wire-table style="table-layout: fixed; width: 100%;">
     {{-- 테이블 제목 --}}
     <x-wire-thead>
-        <th width='50'>cartidx</th>
-        <th width='100'>email</th>
-        <th width='30'>product_id</th>
-        <th width='100'>제품명</th>
-        <th width='100'>이미지</th>
-        <th width='200'>옵션</th>
-        <th width='200'>가격</th>
-        <th width='200'>수량</th>
-        <th width='200'>내용</th>
-        <th width='200'>만료일자</th>
-        <th width='200'>later</th>
-        <th width='200'>수정일자</th>
+        <th style="width: 7%;">cartidx</th>
+        <th style="width: 15%;">이메일</th>
+        <th style="width: 5%;">제품id</th>
+        <th style="width: 10%; word-wrap: break-word;">제품명</th>
+        <th style="width: 20%; word-wrap: break-word;">이미지</th>
+        <th style="width: 5%; word-wrap: break-word;">옵션</th>
+        <th style="width: 8%;">가격</th>
+        <th style="width: 5%;">수량</th>
+        <th style="width: 5%; word-wrap: break-word;">내용</th>
+        <th style="width: 5%;">만료일</th>
+        <th style="width: 5%;">later</th>
+        <th style="width: 10%;">수정일자</th>
     </x-wire-thead>
     <tbody>
         @if(!empty($rows))
             @foreach ($rows as $item)
             {{-- 테이블 리스트 --}}
             <x-wire-tbody-item :selected="$selected" :item="$item">
-                <td width='50'>
+                <td style="width: 7%; overflow: hidden; text-overflow: ellipsis;">
                     <x-click wire:click="edit({{$item->id}})">
                         {{$item->cartidx}}
                     </x-click>
                 </td>
-                <td width='100'>{{$item->email}}</td>
-                <td width='30'>{{$item->product_id}}</td>
-                <td width='100'>{{$item->product}}</td>
-                <td width='100'>{{$item->image}}</td>
-                <td width='200'>{{$item->option}}</td>
-                <td width='200'>{{$item->price}}</td>
-                <td width='200'>{{$item->quantity}}</td>
-                <td width='200'>{{$item->content}}</td>
-                <td width='200'>{{$item->expire}}</td>
-                <td width='200'>{{$item->later}}</td>
-                <td width='200'>{{$item->updated_at}}</td>
+                <td style="width: 15%; overflow: hidden; text-overflow: ellipsis;">{{$item->email}}</td>
+                <td style="width: 5%; overflow: hidden; text-overflow: ellipsis;">{{$item->product_id}}</td>
+                <td style="width: 10%; word-wrap: break-word;">{{$item->product}}</td>
+                <td style="width: 20%; word-wrap: break-word;">{{$item->image}}</td>
+                <td style="width: 5%; word-wrap: break-word;">{{$item->option}}</td>
+                <td style="width: 8%; overflow: hidden; text-overflow: ellipsis;">{{$item->price}}</td>
+                <td style="width: 5%; overflow: hidden; text-overflow: ellipsis;">{{$item->quantity}}</td>
+                <td style="width: 5%; word-wrap: break-word;">{{$item->content}}</td>
+                <td style="width: 5%; overflow: hidden; text-overflow: ellipsis;">{{$item->expire}}</td>
+                <td style="width: 5%; overflow: hidden; text-overflow: ellipsis;">{{$item->later}}</td>
+                <td style="width: 10%; overflow: hidden; text-overflow: ellipsis;">{{$item->updated_at}}</td>
             </x-wire-tbody-item>
             @endforeach
         @endif
     </tbody>
 </x-wire-table>
-
--- resources/views/admin/shop_wish/list.blade.php
+++ resources/views/admin/shop_wish/list.blade.php
@@ -1,37 +1,31 @@
-<x-wire-table>
-    {{-- 테이블 제목 --}}
+<x-wire-table style="table-layout: fixed; width: 100%;">
     <x-wire-thead>
-        <th width='50'>id</th>
-        <th width='100'>이메일</th>
-        <th width='100'>제품 id</th>
-        <th>제품명</th>
-        <th width='100'>가격</th>
-        <th width='100'>이미지</th>
-        <th width='100'>만료일자</th>
-        <th width='100'>later</th>
-        <th width='100'>수정일자</th>
+        <th style="width: 5%;">id</th>
+        <th style="width: 20%;">이메일</th>
+        <th style="width: 10%;">제품 id</th>
+        <th style="width: 25%; word-wrap: break-word;">제품명</th>
+        <th style="width: 10%;">가격</th>
+        <th style="width: 20%; word-wrap: break-word;">이미지</th>
+        <th style="width: 10%;">만료일자</th>
+        <th style="width: 10%;">later</th>
+        <th style="width: 10%;">수정일자</th>
     </x-wire-thead>
     <tbody>
         @if(!empty($rows))
             @foreach ($rows as $item)
             {{-- 테이블 리스트 --}}
             <x-wire-tbody-item :selected="$selected" :item="$item">
-                <td width='50'>
-                    <x-click wire:click="edit({{$item->id}})">
-                        {{$item->id}}
-                    </x-click>
-                </td>
-                <td width='200'>{{$item->email}}</td>
-                <td width='200'>{{$item->product_id}}</td>
-                <td>{{$item->product}}</td>
-                <td width='100'>{{$item->price}}</td>
-                <td width='100'>{{$item->image}}</td>
-                <td width='100'>{{$item->expire}}</td>
-                <td width='100'>{{$item->later}}</td>
-                <td width='100'>{{$item->updated_at}}</td>
+                <td style="overflow: hidden; text-overflow: ellipsis;">{{$item->id}}</td>
+                <td style="overflow: hidden; text-overflow: ellipsis;">{{$item->email}}</td>
+                <td style="overflow: hidden; text-overflow: ellipsis;">{{$item->product_id}}</td>
+                <td style="word-wrap: break-word;">{{$item->product}}</td>
+                <td style="overflow: hidden; text-overflow: ellipsis;">{{$item->price}}</td>
+                <td style="word-wrap: break-word;">{{$item->image}}</td>
+                <td style="overflow: hidden; text-overflow: ellipsis;">{{$item->expire}}</td>
+                <td style="overflow: hidden; text-overflow: ellipsis;">{{$item->later}}</td>
+                <td style="overflow: hidden; text-overflow: ellipsis;">{{$item->updated_at}}</td>
             </x-wire-tbody-item>
             @endforeach
         @endif
     </tbody>
 </x-wire-table>
-
